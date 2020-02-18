c79e5a1270a64fc9eb461e00b8ec9f6cfceda845
Charles Pelton <charliejp0311@gmail.com>
2020-02-18 14:50:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,7 +22,8 @@ class Triangle
     elsif @side_one == @side_two && @side_one == @side_three
       :equilateral
     elsif (@side_one == @side_two && @side_one != @side_three) || (@side_one == @side_three && @side_one != @side_two) || (@side_two == @side_three && @side_two != @side_one)
-
+      :isosceles
+    end
 
   end
 
