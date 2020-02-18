5a2f61a989d07e4c3f246dee44490dfb5aeabd6d
Charles Pelton <charliejp0311@gmail.com>
2020-02-18 16:02:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,7 @@ class Triangle
       raise TriangleError
     elsif @sides.bsearch{|i| i < 0}
       raise TriangleError
-    elsif (@side_one + @side_two < @side_three) || (@side_one + @side_three < @side_two) || (@side_two + @side_three < @side_one)
+    elsif (@side_one + @side_two <= @side_three) || (@side_one + @side_three <= @side_two) || (@side_two + @side_three <= @side_one)
       raise TriangleError
     else
       if @side_one == @side_two && @side_one == @side_three
