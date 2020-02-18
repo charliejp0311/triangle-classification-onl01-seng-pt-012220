ed17ac20252c9ef4bc6ec8cee56980fa1b2489f2
Charles Pelton <charliejp0311@gmail.com>
2020-02-18 14:03:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,13 @@
 class Triangle
   # write code here
+  attr_accessor :sides
+
+  def initialize(side_one, side_two, side_three)
+    
+  end
+
+
+  class TriangleError < StandardError
+    
+  end
 end
