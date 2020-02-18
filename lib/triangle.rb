479a15762270a46c8af2779b1f242b473b859935
Charles Pelton <charliejp0311@gmail.com>
2020-02-18 14:14:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,17 @@
 class Triangle
   # write code here
-  attr_accessor :sides
-
+  attr_accessor :side_one, :side_two, :side_three, :sides 
   def initialize(side_one, side_two, side_three)
-
+    @sides = Array.new(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
   end
 
+  def kind 
+    
+    
+  end
 
   class TriangleError < StandardError
 
