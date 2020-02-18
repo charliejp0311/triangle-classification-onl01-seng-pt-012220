865e328f2b194d0d02189fb2e2958500371807f1
Charles Pelton <charliejp0311@gmail.com>
2020-02-18 15:46:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
   def kind
     if @sides.include?(0)
       raise TriangleError
-    elsif take(@sides) != Array
+    elsif take(@sides)
       raise TriangleError
     else
       if @side_one == @side_two && @side_one == @side_three
