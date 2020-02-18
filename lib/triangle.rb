a6e1d83b0619366caf51f3f403794f67f9547331
Charles Pelton <charliejp0311@gmail.com>
2020-02-18 14:17:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,10 +3,11 @@ class Triangle
   # write code here
   attr_accessor :side_one, :side_two, :side_three, :sides
   def initialize(side_one, side_two, side_three)
-    @sides = Array.new(side_one, side_two, side_three)
+    @sides = []
     @side_one = side_one
     @side_two = side_two
     @side_three = side_three
+    @sides = [side_one, side_two, side_three]
     binding.pry
   end
 
